6ba0ee31dc148e785fe34db4b1b4b565ce2ef8dc
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-04-19 21:40:07 -0700

    moving html file

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/api/notes/:id', (req, res) =>{
 
 
 app.get ('*', (req, res) =>{
-    res.sendFile(path.join (myPath, 'index.html'))
+    res.sendFile(path.join (__dirname, 'index.html'))
 });
 
 app.post('/api/notes', (req, res) =>{
